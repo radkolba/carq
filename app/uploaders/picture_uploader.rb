ca36a647631559d7e035fc18234eac96f526a233
rkolba <rkolba@netkodo.com>
2018-11-29 13:42:30 +0100

    Add user micropost without cloud and with errors caused PictureUploader

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -1,5 +1,13 @@
 class PictureUploader < CarrierWave::Uploader::Base
-  storage :file
+  include CarrierWave::MiniMagick
+  process resize_to_limit: [400, 400]
+
+  if Rails.env.production?
+    storage :file #should be fog but it isn't free
+  else
+    storage :file
+  end
+
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
